2b31d47bd534a8555e4245c12787fb279b9a4d91
s-pc <s-pc@localhost>
2020-10-22 02:22:17 +0530

    p7 commited

---
+++ P7ListCollectionMapping/src/test/java/com/ProgramSeven/P7ListCollectionMapping/AppTest.java
@@ -0,0 +1,20 @@
+package com.ProgramSeven.P7ListCollectionMapping;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
